e94bbc5fc102546d0007a4fd79a0b6ff6726eae5
Collette Patalinghog <patalinghogc@mymacewan.ca>
2024-10-26 18:19:59 -0600

    Added damaged bug sprites and fixed collider issue

--- Assets/Scripts/DestroyBugWithTap.cs
+++ Assets/Scripts/DestroyBugWithTap.cs
@@ -9,12 +9,17 @@ public class DestroyBugWithTap : MonoBehaviour
     private TapGesture gesture;
     public AudioSource bugDeath;
     public AudioClip[] squishSounds;
+    public Sprite[] damagedBug; // damaged bug sprites
+    public int fullHealth; // full health
+    private int health;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<TapGesture>().Tapped += tappedHandler;
-
+        health = fullHealth; // bug's initial health is full health
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -25,10 +30,22 @@ public class DestroyBugWithTap : MonoBehaviour
 
     private void tappedHandler(object sender, System.EventArgs e)
     {
+        health--;
+
         AudioClip randomClip = squishSounds[Random.Range(0, squishSounds.Length)];
         bugDeath.PlayOneShot(randomClip, 0.2f);
-        Debug.Log(randomClip);
-        Invoke("killBug", 0.15f);// Destroy the bug when tapped
+        
+        if (health > 0)
+        {
+            // change sprite to damaged
+            int spriteIndex = Mathf.Clamp(fullHealth - health - 1, 0, damagedBug.Length - 1);
+            spriteRenderer.sprite = damagedBug[spriteIndex];
+        }
+        else
+        {
+            // Destroy the bug when tapped at last health
+            Invoke("killBug", 0.15f);
+        }
     }
 
     private void killBug()
