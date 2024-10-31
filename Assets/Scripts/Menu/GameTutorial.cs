94fa63362de0e1d480bc233142b6f33fe71b9ad2
Collette Patalinghog <patalinghogc@mymacewan.ca>
2024-10-30 20:09:00 -0600

    Added AFK timer for difficulty select overlay

--- Assets/Scripts/Menu/GameTutorial.cs
+++ Assets/Scripts/Menu/GameTutorial.cs
@@ -16,6 +16,7 @@ public class GameTutorial : MonoBehaviour
     private AudioSource soundEffect;
     public float delayBeforeClose = 60f;
 
+    private Coroutine closeOverlayCoroutine;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,6 +33,10 @@ public class GameTutorial : MonoBehaviour
     private void tappedHandler(object sender, EventArgs e)
     {
         soundEffect.Play();
+        // if (closeOverlayCoroutine != null)
+        // {
+        //     StopCoroutine(closeOverlayCoroutine); // stop the running coroutine
+        // }
         Invoke("nextMenu", (float)0.25);
     }
 
@@ -40,18 +45,25 @@ public class GameTutorial : MonoBehaviour
         Instruct.SetActive(true);
         FirstInstruct.SetActive(true);
         // coroutine to start a timer for the instructions to close if left open for 45 seconds
-        StartCoroutine(closeOverlay(delayBeforeClose));
+        //closeOverlayCoroutine = StartCoroutine(closeOverlay(delayBeforeClose));
     }
 
-    IEnumerator closeOverlay(float duration)
-    {
-        yield return new WaitForSeconds(duration);
-        Instruct.SetActive(false);
-        FirstInstruct.SetActive(false);
-        SecondInstruct.SetActive(false);
-        ThirdInstruct.SetActive(false);
-        FourthInstruct.SetActive(false);
-        FifthInstruct.SetActive(false);
-        SixthInstruct.SetActive(false);
-    }
+    // IEnumerator closeOverlay(float duration)
+    // {
+    //     float timer = 0f;
+
+    //     while (timer < duration)
+    //     {
+    //         yield return null; // wait for next frame
+    //         timer += Time.deltaTime; // increment the timer
+    //     }
+    //     yield return new WaitForSeconds(duration);
+    //     Instruct.SetActive(false);
+    //     FirstInstruct.SetActive(false);
+    //     SecondInstruct.SetActive(false);
+    //     ThirdInstruct.SetActive(false);
+    //     FourthInstruct.SetActive(false);
+    //     FifthInstruct.SetActive(false);
+    //     SixthInstruct.SetActive(false);
+    // }
 }
