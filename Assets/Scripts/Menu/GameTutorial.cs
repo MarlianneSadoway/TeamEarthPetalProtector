a611a23cc1c2a01a7556fe5f829170c59fedefdb
Collette Patalinghog <patalinghogc@mymacewan.ca>
2024-10-30 19:11:45 -0600

    Added an AFK timer for menu overlays

--- Assets/Scripts/Menu/GameTutorial.cs
+++ Assets/Scripts/Menu/GameTutorial.cs
@@ -8,7 +8,13 @@ public class GameTutorial : MonoBehaviour
 {
     public GameObject Instruct;
     public GameObject FirstInstruct;
+    public GameObject SecondInstruct;
+    public GameObject ThirdInstruct;
+    public GameObject FourthInstruct;
+    public GameObject FifthInstruct;
+    public GameObject SixthInstruct;
     private AudioSource soundEffect;
+    public float delayBeforeClose = 60f;
 
     // Start is called before the first frame update
     void Start()
@@ -33,5 +39,19 @@ public class GameTutorial : MonoBehaviour
     {
         Instruct.SetActive(true);
         FirstInstruct.SetActive(true);
+        // coroutine to start a timer for the instructions to close if left open for 45 seconds
+        StartCoroutine(closeOverlay(delayBeforeClose));
+    }
+
+    IEnumerator closeOverlay(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        Instruct.SetActive(false);
+        FirstInstruct.SetActive(false);
+        SecondInstruct.SetActive(false);
+        ThirdInstruct.SetActive(false);
+        FourthInstruct.SetActive(false);
+        FifthInstruct.SetActive(false);
+        SixthInstruct.SetActive(false);
     }
 }
