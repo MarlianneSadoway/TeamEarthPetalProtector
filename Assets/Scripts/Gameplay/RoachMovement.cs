5084316f5afd1caf5d5d741ea68131d3a3369033
MarlianneSadoway <136766734+MarlianneSadoway@users.noreply.github.com>
2024-11-14 09:30:40 -0700

    changed ant to "roach"

--- Assets/Scripts/Gameplay/RoachMovement.cs
+++ Assets/Scripts/Gameplay/RoachMovement.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AntMovement : MonoBehaviour
+public class RoachMovement : MonoBehaviour
 {
-    [Header("Ant Configuration")]
-    public float speed = 1f; // Speed of the ant
-    private Rigidbody2D rb; // The ant's Rigidbody2D component
-    public Transform target;
+    [Header("Roach Configuration")]
+    public float speed = 0.8f; // Speed of the roach
+    private Rigidbody2D rb; // The roach's Rigidbody2D component
     public HealthController healthController; // Reference to the HealthController script
     public Transform spawnRoot; // Transform to link bugs to prefab instance
     private bool hasDamagedPlant = false; // Flag to prevent multiple heart losses
@@ -16,27 +15,27 @@ public class AntMovement : MonoBehaviour
     void Start()
     {
         spawnRoot = gameObject.transform.parent.transform;
-        // Set the ant's initial localPosition at the left edge of the screen, just above the dirt 
+        // Set the roach's initial localPosition at the left edge of the screen, just above the dirt 
         transform.localPosition = new Vector3(-2.78f, -1.999926f, 0f); // Y is on top of the dirt
 
-        // Get the ant's Rigidbody2D component
+        // Get the roach's Rigidbody2D component
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Move the ant horizontally to the right
+        // Move the roach horizontally to the right
         transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
 
-        // Destroy the ant if it has gone off-screen
+        // Destroy the roach if it has gone off-screen
         if (transform.localPosition.y < -2f || transform.localPosition.x < -8f || transform.localPosition.x > 8f || transform.localPosition.y > 10f)
         {
             Destroy(gameObject);
         }
     }
 
-    // OnTriggerEnter2D is called when the ant collides with the plant
+    // OnTriggerEnter2D is called when the roach collides with the plant
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Plant") && !hasDamagedPlant)
@@ -47,10 +46,10 @@ public class AntMovement : MonoBehaviour
                 healthController.RemoveHeart();
             }
 
-            // Set flag that bug has damaged plant so that it can't damage plant again 
+            // Set flag that roach has damaged plant so that it can't damage plant again 
             hasDamagedPlant = true;
 
-            // Destroy ant because it has reached the plant
+            // Destroy roach because it has reached the plant
             Destroy(gameObject);
 
         }
