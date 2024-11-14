654b7dc999628a2d9c9e657a1cd8f3697dec4ff6
MarlianneSadoway <136766734+MarlianneSadoway@users.noreply.github.com>
2024-11-14 09:46:27 -0700

    Started Hard difficulty

---
+++ Assets/Scripts/Menu/PlayHard.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TouchScript.Gestures;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayHard : MonoBehaviour
+{
+    public GameObject SelectDiff;
+    private AudioSource soundEffect;
+    public GameIndex rootIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundEffect = GetComponent<AudioSource>();
+        GetComponent<TapGesture>().Tapped += tappedHandler;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void tappedHandler(object sender, EventArgs e)
+    {
+        soundEffect.Play();
+        //Invoke("nextMenu", (float)0.25);
+        rootIndex.Invoke("swapGameHard", 0.25f);
+    }
+
+    private void nextMenu()
+    {
+        SelectDiff.SetActive(false); // deactivates overlay before loading next scene
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single); // loads MainScene when Hard is tapped
+    }
+
+}
